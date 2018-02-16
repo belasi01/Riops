b51e12541b4722c0547db85f298d9568afad6e0d
simon belanger <simonbelanger@iMac-de-simon.local>
2018-02-15 21:22:04 -0500

    Update documentation

--- R/spectral.aph.bricaud.R
+++ R/spectral.aph.bricaud.R
@@ -1,16 +1,24 @@
-#' Compute spectral phyotplankton absorption
-#' using Bricaud et al (1995, 1998) statistics
-#' for case 1 waters
+#'
+#'@title Case-1 waters absorption coefficient for Particles and Phyotoplankton
+#'
+#'
+#'@description Compute spectral absorption coefficients for particles and phytoplankton
+#' using Bricaud et al (1995, 1998) statistics established for case 1 waters
+#'
+#' @param waves is the wavelenght (could be a vector)
+#' @param chl is the chlorophyll-a concentration
+#' @param PHYTO is a logical to compute either phytoplankton
+#' or total particles coefficient. Default is PHYTO=TRUE to return the phytoplankton
+#' pigments spectrum.
+#'
+#'
+#'@author Simon Bélanger
+#'
+#'@export
 
-
-spectral.aph.bricaud <- function(wl,chl) {
-
-
-  A_phy <-  spline(AphyTable$V1, AphyTable$V2,  xout=wl, method="natural")
-
-  E_phy <-  spline(AphyTable$V1, AphyTable$V3,  xout=wl, method="natural")
-
-  aPHY <- A_phy$y * chl ^ E_phy$y
-
-  return(aPHY)
+spectral.aph.bricaud <- function(waves,chl, PHYTO=TRUE) {
+  A_phy <-  spline(AphyTable$V1, AphyTable$V2,  xout=waves, method="natural")
+  A_p <-  spline(AphyTable$V1, AphyTable$V4,  xout=waves, method="natural")
+  E_phy <-  spline(AphyTable$V1, AphyTable$V3,  xout=waves, method="natural")
+  if (PHYTO) return(A_phy$y * chl ^ E_phy$y) else return(A_p$y * chl ^ E_phy$y)
 }
--- R/spectral.bw.R
+++ R/spectral.bw.R
@@ -1,13 +1,18 @@
-# TODO: Add comment
-#
-# Author: Simon
-###############################################################################
+#'
+#'@title Pure water scattering coefficient spectrum
+#'
+#'
+#'@description Compute spectral scattering coefficient for pure water Morel 1974 (as summarized by Mobley 1994)
+#'
+#' @param waves is the wavelenght (could be a vector)
+#'
+#'@author Simon Bélanger
+#'
+#'@export
 
-
-
-spectral.bw <- function(WL)
+spectral.bw <- function(waves)
 {
-	bw <- 16.06*4.72e-4*(400./WL)^4.32
+	bw <- 16.06*4.72e-4*(400./waves)^4.32
 	return(bw)
 }
 
